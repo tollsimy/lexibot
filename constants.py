da878666cd165b4eadf9e2605a2724e2a3797b7c
tollsimy <simonetollardo@gmail.com>
2022-10-22 16:45:45 +0200

    Updates

--- constants.py
+++ constants.py
@@ -1,13 +1,4 @@
-help_msg = """
-Welcome, the LexiBox help message is here to guide you!
-Send /start - start and configure the bot.
-Send /help - get this message.
-Send /set_lang [language_to_learn] to [mothertongue] - set the language (e.g. /set_lang it to en).
-Send /set_sheet [link] - set the google sheet link (e.g. /set_sheet https://docs.google.com/spreadsheets/d/example).
-Send /set_email [your_email@gmail.com] - set the google email address to whom the sheet will be shared.
-After setting the language and the sheet, send whatever you want to translate to the bot and it will append the translation to the sheet.
-The intended use of the LexiBot is to translate a new words whenever you encounter one and then store them all in a google sheet along with the original words in order to study and memorize them in a clever way.
-"""
+import os
 
 lang_dict={
     "english" : "en", 
@@ -68,3 +59,33 @@ lang_dict={
     #TODO: missing chinese
     #TODO: don't know if they are all supported by reverso
 }
+
+scripts_path = os.getcwd() + os.path.abspath("/scripts-and-automations/reverso-script")
+
+token = "keys/lexibot_token.txt"
+gservice_acc = "keys/lexibot_gservice_account.json"
+
+help_msg = """
+Welcome, the LexiBox help message is here to guide you!
+Send /start - start and configure the bot.
+Send /help - get this message.
+Send /set_lang [language_to_learn] to [mothertongue] - set the language (e.g. /set_lang it to en).
+Send /set_sheet [link] - set the google sheet link (e.g. /set_sheet https://docs.google.com/spreadsheets/d/example).
+Send /set_email [your_email@gmail.com] - set the google email address to whom the sheet will be shared.
+After setting the language and the sheet, send whatever you want to translate to the bot and it will append the translation to the sheet.
+The intended use of the LexiBot is to translate a new words whenever you encounter one and then store them all in a google sheet along with the original words in order to study and memorize them in a clever way.
+"""
+
+welcome_msg = """
+Hi, I'm LexiBot, I can help you to learn vocabulary in a foreign language.
+The intended use of the LexiBot is to translate a new words whenever you encounter one and then store them all in a google sheet along with the original words in order to study and memorize them in a clever way.
+Send /cancel to stop conversation and manually set LexiBot.
+"""
+guided_msg_lang = """
+First of all, you need to tell me which language you want to learn and which is your mothertongue.
+Format: [language_to_learn] to [mothertongue] (e.g. italian to english)
+"""
+guided_msg_email = "Wonderful! Now send me your google email address to share with you the sheet that will store your words (e.g. namesurname@gmail.com)"
+guided_msg_sheet = "Last step! Now send me the `create [sheet_name]` or `resume [sheet_id]` to respectively create a new google sheet or resume an existing one."
+guided_msg_complete = "Great! Now you can start to translate words, just send me a message and I'll reply with the translation while writing it on your sheet to keep track of it."
+guided_msg_exit = "Guided config ended, you can now use /set_lang and /set_sheet and /set_email to manually set LexiBot"
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # -------------------------- Imports and basic stuff ------------------------- #
 import logging
+import re
 import constants as const
 from telegram import __version__ as TG_VER
 import telegram as telegram
 import sys 
-import os
-sys.path.append(os.getcwd() + os.path.abspath("/scripts-and-automations/reverso-script"))
+sys.path.append(const.scripts_path)
 from reverso_wrapped_api import *
 from gsheets_wrapped_api import *
 
 TOKEN = None
-with open("keys/lexibot_token.txt") as f:
+with open(const.token) as f:
     TOKEN = f.read().strip()
 
 try:
@@ -43,9 +43,11 @@ sheet_name = None
 sheet_action = None
 target_lang = None
 mother_lang = None
+email = None
+
+# Class instances
 reverso = None
 gsheet = None
-email = None
 
 # Conversation FSM states
 LANG_STATE = 0
@@ -55,8 +57,16 @@ SHEET_STATE = 2
 # Sheet access methods
 OPEN_BY_NAME = 0
 OPEN_BY_ID = 1
+
+# ----------------------------- Utility functions ----------------------------- #
+
+#TODO
+def printall(gsheet):
+    pass
+
 # ----------------------------- Command Handlers ----------------------------- #
 
+# /start command handler
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     #Send a message when the command /start is issued
     user = update.effective_user
@@ -74,15 +84,16 @@ async def set_lang(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     global mother_lang
     global reverso
 
+    # Langauges are case-insensitive
     msg = update.message.text.lower()
 
     try:
-        if("/set_lang" in update.message.text):
+        if("/set_lang" in msg):
             arg1 = msg.split("/set_lang ")[1]
         else:
-            arg1 = msg.lower()
+            arg1 = msg
         arg1 = arg1.split(" ")[0]
-        arg2 = msg.lower().split(" to ")[1]
+        arg2 = msg.split(" to ")[1]
         logger.debug("Languages set to " + arg1 + " to " + arg2)
     except:
         # If we are here from the command handler, message is this one
@@ -126,10 +137,12 @@ async def set_lang(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 async def set_email(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     
     global email
+
+    # Email are case-insensitive
     msg = update.message.text.lower()
 
     try:
-        if("/set_email" in update.message.text):
+        if("/set_email" in msg):
             email = msg.split("/set_email ")[1]
         else:
             email = msg
@@ -161,89 +174,75 @@ async def set_sheet(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     msg = update.message.text
 
-    #TODO: handle uppercase letters in "create" or "resume"
     try:
-        if("/set_sheet" in msg):
-            if "create" in msg.lower():
-                sheet_name = msg.split("/set_sheet create ")[1]
+        if("/set_sheet" in msg.lower()):
+            if ("create" in msg.lower()):
+                sheet_name = re.split("/set_sheet create ", msg, flags=re.IGNORECASE)[1]
+                sheet_name = sheet_name + " - LexiBot, learn new vocabularies"
                 sheet_action = OPEN_BY_NAME
                 logger.debug("Create new sheet: " + sheet_name)
-            elif "resume" in msg.lower():
-                sheet_id = msg.split("/set_sheet resume ")[1]
+            elif ("resume" in msg.lower()):
+                sheet_id = re.split("/set_sheet resume ", msg, flags=re.IGNORECASE)[1]
                 sheet_action = OPEN_BY_ID
                 logger.debug("Resume sheet: " + sheet_id)
             else:
-                raise Exception
+                raise SyntaxError
         else:
-            if "create" in msg.lower():
-                sheet_name = msg.split("create ")[1]
+            if ("create" in msg.lower()):
+                sheet_name = re.split("create ", msg, flags=re.IGNORECASE)[1]
+                sheet_name = sheet_name + " - LexiBot, learn new vocabularies"
                 sheet_action = OPEN_BY_NAME
                 logger.debug("Create new sheet: " + sheet_name)
-            elif "resume" in msg.lower():
-                sheet_id = msg.split("resume ")[1]
+            elif ("resume" in msg.lower()):
+                sheet_id = re.split("resume ", msg, flags=re.IGNORECASE)[1]
                 sheet_action = OPEN_BY_ID
                 logger.debug("Resume sheet: " + sheet_id)
             else:
-                raise Exception
-    except:
+                raise SyntaxError
+    # IndexError returned if re.split doesn't find the substring
+    except SyntaxError or IndexError:
         # If we are here from the command handler, message is this one
         if("/set_sheet" in update.message.text):
             await update.message.reply_text("Invalid command, use /set_sheet create [sheet_name] or /set_sheet resume [sheet_id]")
         # If we are here from the conversation handler, message is different
         else:
             await update.message.reply_text("Invalid input, send create [sheet_name] or resume [sheet_id]")
-        logger.debug("/set_sheet: Invalid command")
+        logger.exception("/set_sheet: Invalid command exception")
         return -1
 
     # Start gsheet
     try:
-        gsheet = Gsheet_Api("keys/lexibot_gservice_account.json")
+        gsheet = Gsheet_Api(const.gservice_acc)
         logger.debug("Gsheet service account authorized correctly")
     except:
         logger.exception("Error while connecting to google service account")
+        await update.message.reply_text("Error while connecting to google service, please contact the bot maintainers [Tollsimy](t.me/Tollsimy) and/or [Cannox227](t.me/Cannox227).", parse_mode=telegram.constants.ParseMode.MARKDOWN)
+        return -2
+
+    if email is None:
+        await update.message.reply_text("Please insert an email first")
+        logger.debug("Please insert an email first")
         return -1
 
-    #TODO
-    if 1:
-        success = False
-        sheet_name = "LexiBot - learn new vocabularies"
-        if sheet_action == OPEN_BY_NAME:
-            gsheet.create_sheet(sheet_name)
-            logger.debug(f"Created Sheet {gsheet.sh.id}")
-            await update.message.reply_text(f"Created Sheet named: {sheet_name}, id:`{gsheet.sh.id}`", parse_mode=telegram.constants.ParseMode.MARKDOWN)
-            success = True
-        elif sheet_action == OPEN_BY_ID:
-            if gsheet.is_a_g_sheet("key", sheet_id):
-                logger.debug(f"Sheet {sheet_id} is a valid google sheet")
-                success = True
-            else:
-                logger.debug(f"Sheet {sheet_id} is not a valid google sheet")
-        if success:
-            if email is not None:
-                gsheet.sh.share(email, perm_type='user', role='writer')
-                logger.debug("Sheet created correctly")
-            else:
-                await update.message.reply_text("Please insert an email first")
-                logger.debug("Please insert an email first")
-        #await update.message.reply_text(f"")
-        
-        #logger.debug(sheet_id)
-        
-    else:
-        #TODO
-        pass
-
-    # if gsheet.is_a_g_sheet(title, sheet) :
-    #     logger.debug("Sheet link is valid")
-    #     sheet=arg
-    #     await update.message.reply_text("Sheet link set to: " + sheet + "RETRIEVE NAME OF THE SHEET")
-    # else:
-    #     logger.debug("Sheet link not valid")
-    #     await update.message.reply_text("This is not a link to a google sheet document, please try again")
-    #     return -1
-
+    if sheet_action == OPEN_BY_NAME:
+        gsheet.create_sheet(sheet_name)
+        logger.debug(f"Created Sheet {gsheet.sh.id}")
+        gsheet.sh.share(email, perm_type='user', role='writer')
+        logger.debug("Sheet shared correctly")
+        await update.message.reply_text(f"Created Sheet named: {sheet_name}, id:`{gsheet.sh.id}`", parse_mode=telegram.constants.ParseMode.MARKDOWN)
+    elif sheet_action == OPEN_BY_ID:
+        if gsheet.is_a_g_sheet("key", sheet_id):
+            logger.debug(f"Sheet {sheet_id} is a valid google sheet")
+            gsheet.sh.share(email, perm_type='user', role='writer')
+            logger.debug("Sheet shared correctly")
+            await update.message.reply_text(f"Sheet id:`{gsheet.sh.id}` resumed correctly!", parse_mode=telegram.constants.ParseMode.MARKDOWN)
+        else:
+            logger.debug(f"Sheet {sheet_id} is not a valid google sheet")
+            await update.message.reply_text("sheet_id not valid, please retry")
+            return -1
     return 0
 
+# Handler for whatever text that is not a command
 async def translate(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     #Reply with the translated the user message
     if(target_lang is None or mother_lang is None):
@@ -266,23 +265,57 @@ async def translate(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Done! Translation: " + trad)
     return 0
 
+async def get_target_lang(update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if(mother_lang is not None):
+        await update.message.reply_text("Mother tonge language set to: " + mother_lang)
+        logging.debug("get_target_lang called: " + mother_lang)
+    else:
+        await update.message.reply_text("Mother tonge language not set")
+        logging.debug("get_target_lang called: mother tongue not set")
+
+async def get_mother_lang(update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if(target_lang is not None):
+        await update.message.reply_text("Target language set to: " + target_lang)
+        logging.debug("get_mother_lang called: " + target_lang)
+    else: 
+        await update.message.reply_text("Target language not set")
+        logging.debug("get_mother_lang called: target language not set")
+
+async def get_sheet(update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if(sheet_id is not None):
+        await update.message.reply_text("Sheet link set to: " + sheet_id)
+        logging.debug("get_sheet called: " + sheet_id)
+    else:
+        await update.message.reply_text("Sheet link not set")
+        logging.debug("get_sheet called: sheet link not set")
+
+async def get_email(update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if(email is not None):
+        await update.message.reply_text("Email address set to: " + email)
+        logging.debug("get_email called: " + email)
+    else:
+        await update.message.reply_text("Email address not set")
+        logging.debug("get_email called: email address not set")
+
+async def random(update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if(gsheet is not None):
+        gsheet.
+
+async def printall(update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    
+
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Command not found, visit /help")
 
+
 # -------------------------- Conversation Handlers --------------------------- #
 
+# Handler for /start command guided setup
 async def start_main_conv(update, context: ContextTypes.DEFAULT_TYPE):
     # Start main conversation
-    await update.message.reply_text("""
-Hi, I'm LexiBot, I can help you to learn vocabulary in a foreign language.
-The intended use of the LexiBot is to translate a new words whenever you encounter one and then store them all in a google sheet along with the original words in order to study and memorize them in a clever way.
-Send /cancel to stop conversation and manually set LexiBot.
-""")
+    await update.message.reply_text(const.welcome_msg)
     logging.debug("Start main conversation")
-    await update.message.reply_text("""
-First of all, you need to tell me which language you want to learn and which is your mothertongue.
-Format: [language_to_learn] to [mothertongue] (e.g. italian to english)
-""")
+    await update.message.reply_text(const.guided_msg_lang)
     return LANG_STATE
 
 async def ask_lang(update, context: ContextTypes.DEFAULT_TYPE):
@@ -290,7 +323,7 @@ async def ask_lang(update, context: ContextTypes.DEFAULT_TYPE):
     ret = await set_lang(update, context)
     if(ret == -1):
         return LANG_STATE
-    await update.message.reply_text("Wonderful! Now send me your google email address to share with you the sheet that will store your words (e.g. namesurname@gmail.com)")
+    await update.message.reply_text(const.guided_msg_email)
     return EMAIL_STATE
 
 async def ask_email(update, context: ContextTypes.DEFAULT_TYPE):
@@ -298,7 +331,7 @@ async def ask_email(update, context: ContextTypes.DEFAULT_TYPE):
     ret = await set_email(update, context)
     if(ret == -1):
         return EMAIL_STATE
-    await update.message.reply_text("Last step! Now send me the `create [sheet_name]` or `resume [sheet_id]` to respectively create a new google sheet or resume an existing one.")  
+    await update.message.reply_text(const.guided_msg_sheet)  
     return SHEET_STATE
 
 async def ask_sheet(update, context: ContextTypes.DEFAULT_TYPE):
@@ -306,48 +339,14 @@ async def ask_sheet(update, context: ContextTypes.DEFAULT_TYPE):
     ret = await set_sheet(update, context)
     if(ret == -1):
         return SHEET_STATE
-    await update.message.reply_text("Great! Now you can start to translate words, just send me a message and I'll reply with the translation while writing it on your sheet to keep track of it.")
-    return ConversationHandler.END
-
-async def get_target_lang(update, context: ContextTypes.DEFAULT_TYPE):
-    if(mother_lang is not None):
-        await update.message.reply_text("Mother tonge language set to: " + mother_lang)
-        logging.debug("get_target_lang called: " + mother_lang)
-    else:
-        await update.message.reply_text("Mother tonge language not set")
-        logging.debug("get_target_lang called: mother tongue not set")
-    return ConversationHandler.END
-
-async def get_mother_lang(update, context: ContextTypes.DEFAULT_TYPE):
-    if(target_lang is not None):
-        await update.message.reply_text("Target language set to: " + target_lang)
-        logging.debug("get_mother_lang called: " + target_lang)
-    else: 
-        await update.message.reply_text("Target language not set")
-        logging.debug("get_mother_lang called: target language not set")
-    return ConversationHandler.END
-
-async def get_sheet(update, context: ContextTypes.DEFAULT_TYPE):
-    if(sheet_id is not None):
-        await update.message.reply_text("Sheet link set to: " + sheet_id)
-        logging.debug("get_sheet called: " + sheet_id)
-    else:
-        await update.message.reply_text("Sheet link not set")
-        logging.debug("get_sheet called: sheet link not set")
-    return ConversationHandler.END
-
-async def get_email(update, context: ContextTypes.DEFAULT_TYPE):
-    if(email is not None):
-        await update.message.reply_text("Email address set to: " + email)
-        logging.debug("get_email called: " + email)
-    else:
-        await update.message.reply_text("Email address not set")
-        logging.debug("get_email called: email address not set")
+    elif(ret == -2):
+        return ConversationHandler.END
+    await update.message.reply_text(const.guided_msg_complete)
     return ConversationHandler.END
 
 async def cancel(update, context: ContextTypes.DEFAULT_TYPE):
     # Quit conversation
-    await update.message.reply_text('Guided config ended, you can now use /set_lang and /set_sheet and /set_email to manually set LexiBot')
+    await update.message.reply_text(const.guided_msg_exit)
     logging.debug("/cancel called: Guided config ended") #TODO: perchè non viene eseguito?
     return ConversationHandler.END
 
@@ -360,30 +359,35 @@ def main() -> None:
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(TOKEN).build()
     
-    #Main conversation handler
+    # /start guided setup conversation
     main_conv_handler = ConversationHandler(
+        # ENtry point is /start command
         entry_points=[CommandHandler('start', start_main_conv)],
-
+        # States are Lang setup, Email setupn and GSheet setup
         states={
             LANG_STATE : [MessageHandler(filters.TEXT & ~filters.Command(['/cancel']), ask_lang)],
             EMAIL_STATE : [MessageHandler(filters.TEXT & ~filters.Command(['/cancel']), ask_email)],
             SHEET_STATE : [MessageHandler(filters.TEXT & ~filters.Command(['/cancel']), ask_sheet)],
         },
-
+        # /cancel command to exit guided setup
         fallbacks=[CommandHandler("cancel", cancel)]
     )
     application.add_handler(main_conv_handler)
 
+    # Manual commands
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("set_lang", set_lang))
+    application.add_handler(CommandHandler("set_email", set_email))
     application.add_handler(CommandHandler("set_sheet", set_sheet))
     application.add_handler(CommandHandler("get_target_lang", get_target_lang))
     application.add_handler(CommandHandler("get_mother_lang", get_mother_lang))
     application.add_handler(CommandHandler("get_sheet", get_sheet))
     application.add_handler(CommandHandler("get_email", get_email))
-    # translate message
+    application.add_handler(CommandHandler("random", random))
+    application.add_handler(CommandHandler("printall", printall))
+    # Translate message
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, translate))
-    # unknown command handler, must be added last
+    # Unknown command handler, must be added last
     application.add_handler(MessageHandler(filters.COMMAND, unknown))
 
     # Run the bot
@@ -392,6 +396,8 @@ def main() -> None:
 if __name__ == "__main__":
     main()
 
-# TODO: fare in modo che inviando il comando /set_lang ecc senza argomenti chieda di inserirli al messaggio seguente
+# TODO: Google sheet must not have duplicates
+# TODO: gestire spazi multipli nei comandi -> regex?
+# TODO: Aggiungere debug msg alla console con log level appropriati
+# TODO: fare in modo che inviando ad esempio il comando /set_lang ecc senza argomenti chieda di inserirli al messaggio seguente -> conv per ogni comando?
 # TODO: fare comando che restituisce tutti i record della sheet e un comando che restituisce parola randomica da studiare
-# TODO: togliere il token prima di committare
